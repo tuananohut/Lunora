c34bac0c8e25c8236d683f07fa829c337d7527ae
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-01-27 16:01:29 +0300

    dx11 device created

--- source/engine.cpp
+++ source/engine.cpp
@@ -2,57 +2,93 @@
 #define UNICODE
 #endif 
 
-// https://www.flaticon.com/free-icon/cube_9853406?term=shape&page=1&position=93&origin=search&related_id=9853406
-
 #include <windows.h>
+#include "renderer.h"
+
+const wchar_t CLASS_NAME[] = L"Engine";
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-    const wchar_t CLASS_NAME[] = L"Engine";
+    WNDCLASS wc = { };
+    
     HICON hIcon = (HICON)LoadImage(NULL, L"source/lunora.ico", IMAGE_ICON, 32, 32, LR_LOADFROMFILE); 
     DWORD wStyles = WS_EX_ACCEPTFILES | WS_EX_TOOLWINDOW | WS_OVERLAPPEDWINDOW;
+    WCHAR szExePath[MAX_PATH];
+    GetModuleFileName(NULL, szExePath, MAX_PATH);
 
-    WNDCLASS wc = { };
-
-    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
     wc.lpfnWndProc = WindowProc;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hInstance = hInstance;
     wc.hIcon = hIcon;
-    wc.hCursor = 0;
-    wc.hbrBackground = HBRUSH(COLOR_WINDOW);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
     wc.lpszMenuName = NULL; 
     wc.lpszClassName = CLASS_NAME;
 
-    RegisterClass(&wc);
+    if (!RegisterClass(&wc))
+    {
+        DWORD dwError = GetLastError();
+        if (dwError != ERROR_CLASS_ALREADY_EXISTS)
+        {
+            return HRESULT_FROM_WIN32(dwError);
+        }
+    }
+
+    RECT m_rc; 
+
+    int x = CW_USEDEFAULT;
+    int y = CW_USEDEFAULT;
+
+    HMENU hMenu = NULL; 
+
+    int nDefaultWidth = 640;
+    int nDefaultHeight = 480;
+    SetRect(&m_rc, 0, 0, nDefaultWidth, nDefaultHeight);
+    AdjustWindowRect(&m_rc, WS_OVERLAPPEDWINDOW, (hMenu != NULL) ? true : false);
 
     HWND hwnd = CreateWindowEx(0,                   
                                CLASS_NAME,          
                                L"Physics Engine",  
                                wStyles,        
-                               CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+                               x, y, (m_rc.right - m_rc.left), (m_rc.bottom - m_rc.top),
                                NULL,       
-                               NULL,       
+                               hMenu,       
                                hInstance,  
                                NULL);
 
     if (hwnd == NULL)
     {
-        return 0;
+        DWORD dwError = GetLastError();
+        return HRESULT_FROM_WIN32(dwError);
     }
 
     ShowWindow(hwnd, nCmdShow);
 
-    // Run the message loop.
-
+    bool bGotMsg; 
     MSG msg = { };
-    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    msg.message = WM_NULL; 
+    PeekMessage(&msg, NULL, 0U, 0U, PM_NOREMOVE);
+
+    while (WM_QUIT != msg.message)
     {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        bGotMsg = (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE) != 0);
+
+        if (bGotMsg)
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        else
+        {
+            // renderer->Update(); 
+            // renderer->Render();
+            // deviceResources->Present();
+        }
     }
 
     return 0;
@@ -62,6 +98,20 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
+
+    case WM_CLOSE: 
+    {
+        HMENU hMenu;
+        hMenu = GetMenu(hwnd);
+        if (hMenu != NULL)
+        {
+            DestroyMenu(hMenu);
+        }
+
+        DestroyWindow(hwnd);
+        // UnregisterClass(CLASS_NAME, m_hInstance);
+        return 0;
+    }
 
     case WM_DESTROY:
     {
---
+++ source/renderer.h
@@ -0,0 +1,47 @@
+#ifndef _RENDERER_H_
+#define _RENDERER_H_
+
+#include <d3d11.h>
+#include <WinUser.h>
+#include <wrl/client.h>
+
+class RenderManager
+{
+	RenderManager()
+	{
+		D3D_FEATURE_LEVEL levels[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_11_1 };
+		D3D_FEATURE_LEVEL m_featureLevel = levels[0];
+		UINT deviceFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
+		HRESULT hr;
+
+#if defined(DEBUG) || defined(_DEBUG)
+		deviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif;
+
+		// ID3D11Device* device;
+		// ID3D11DeviceContext* deviceContext;
+		Microsoft::WRL::ComPtr<ID3D11Device> device;
+		Microsoft::WRL::ComPtr<ID3D11DeviceContext> deviceContext;
+
+		hr = D3D11CreateDevice(nullptr,
+			D3D_DRIVER_TYPE_HARDWARE,
+			0,
+			deviceFlags,
+			levels,
+			ARRAYSIZE(levels),
+			D3D11_SDK_VERSION,
+			&device,
+			&m_featureLevel,
+			&deviceContext);
+
+		if (FAILED(hr))
+		{
+
+		}
+
+	
+	};
+};
+
+
+#endif
