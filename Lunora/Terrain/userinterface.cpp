ba41135dade06183d1a3f430cc090e508c2f7aac
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-05-27 23:05:00 +0300

    ui for terrain

--- Lunora/Terrain/userinterface.cpp
+++ Lunora/Terrain/userinterface.cpp
@@ -155,3 +155,242 @@ bool UserInterface::Initialize(D3D* Direct3D,
 
   return true;
 }
+
+void UserInterface::Shutdown()
+{
+  if (m_PositionStrings)
+    {
+      m_PositionStrings[0].Shutdown();
+      m_PositionStrings[1].Shutdown();
+      m_PositionStrings[2].Shutdown();
+      m_PositionStrings[3].Shutdown();
+      m_PositionStrings[4].Shutdown();
+      m_PositionStrings[5].Shutdown();
+
+      delete[] m_PositionStrings;
+      m_PositionStrings = nullptr; 
+    }
+
+  if (m_VideoStrings)
+    {
+      m_VideoStrings[0].Shutdown();
+      m_VideoStrings[1].Shutdown();
+
+      delete[] m_VideoStrings;
+      m_VideoStrings = nullptr; 
+    }
+
+  if (m_FpsString)
+    {
+      m_FpsString->Shutdown();
+      delete m_FpsString;
+      m_FpsString = nullptr; 
+    }
+
+  if (m_Font1)
+    {
+      m_Font1->Shutdown();
+      delete m_Font1;
+      m_Font1 = nullptr; 
+    }
+}
+
+bool UserInterface::Frame(ID3D11DeviceContext* deviceContext,
+			  int fps,
+			  float posX, float posY, float posZ,
+			  float rotX, float rotY, float rotZ)
+{
+  bool result;
+
+  result = UpdateFpsString(deviceContext, fps);
+  if (!result)
+    {
+      return false; 
+    }
+
+  result = UpdatePositionStrings(deviceContext, posX, posY, posZ, rotX, rotY, rotZ);
+  if (!result)
+    {
+      return false; 
+    }
+
+  return true; 
+}
+
+bool UserInterface::Render(D3D* Direct3D,
+			   ShaderManager* ShaderManager,
+			   XMMATRIX worldMatrix,
+			   XMMATRIX viewMatrix,
+			   XMMATRIX projectionMatrix)
+{
+  int i;
+
+  Direct3D->TurnZBufferOff();
+  Direct3D->EnableAlphaBlending();
+
+  m_FpsString->Render(Direct3D->GetDeviceContext(), ShaderManager, worldMatrix, viewMatrix, orthoMatrix, m_Font1->GetTexture());
+
+  m_VideoStrings[0].Render(Direct3D->GetDeviceContext(), ShaderManager, worldMatrix, viewMatrix, orthoMatrix, m_Font1->GetTexture());
+  m_VideoStrings[1].Render(Direct3D->GetDeviceContext(), ShaderManager, worldMatrix, viewMatrix, orthoMatrix, m_Font1->GetTexture());
+
+  for(i = 0; i < 6; i++)
+    {
+      m_PositionStrings[i].Render(Direct3D->GetDeviceContext(), ShaderManager, worldMatrix, viewMatrix, orthoMatrix, m_Font1->GetTexture());
+    }
+
+  Direct3D->DisableAlphaBlending();
+
+  Direct3D->TurnZBufferOn();
+
+  return true; 
+}
+
+bool UserInterface::UpdateFpsString(ID3D11DeviceContext* deviceContext, int fps)
+{
+  char tempString[16];
+  char finalString[16];
+  float red, green, blue;
+  bool result;
+
+  if (m_previousFps == fps)
+    {
+      return true; 
+    }
+
+  m_previousFps = fps;
+
+  if (fps > 99999)
+    {
+      fps = 99999;
+    }
+
+  _itoa_s(fps, tempString, 10);
+
+  strcpy_s(finalString, "Fps: ");
+  strcat_s(finalString, tempString);
+
+  if (fps >= 60)
+    {
+      red = 0.f;
+      green = 1.f;
+      blue = 0.f; 
+    }
+
+  if (fps < 60)
+    {
+      red = 1.f;
+      green = 1.f;
+      blue = 0.f; 
+    }
+
+  if (fps < 30)
+    {
+      red = 1.f;
+      green = 0.f;
+      blue = 0.f; 
+    }
+
+  result = m_FpsString->UpdateSentence(deviceContext, m_Font1, finalString, 10, 50, red, green, blue);
+  if (!result)
+    {
+      return false; 
+    }
+
+  return true; 
+}
+
+bool UserInterface::UpdatePositionStrings(ID3D11DeviceContext* deviceContext,
+					  float posX, float posY, float posZ,
+					  float rotX, float rotY, float rotZ)
+{
+  int positionX, positionY, positionZ, rotationX, rotationY, rotationZ;
+  char tempString[16];
+  char finalString[16];
+  bool result;
+
+  positionX = (int)posX;
+  positionY = (int)posY;
+  positionZ = (int)posZ;
+  rotationX = (int)rotX;
+  rotationY = (int)rotY;
+  rotationZ = (int)rotZ;
+
+  if (positionX != m_previousPosition[0])
+    {
+      m_previousPosition[0] = positionX;
+      _itoa_s(positionX, tempString, 10);
+      strcpy_s(finalString, "X: ");
+      strcat_s(finalString, tempString);
+      result = m_PositionStrings[0].UpdateSentence(deviceContext, m_Font1, finalString, 10, 100, 1.f, 1.f, 1.f);
+      if (!result)
+	{
+	  return false; 
+	}
+    }
+  
+  if (positionY != m_previousPosition[1])
+    {
+      m_previousPosition[1] = positionY;
+      _itoa_s(positionY, tempString, 10);
+      strcpy_s(finalString, "Y: ");
+      strcat_s(finalString, tempString);
+      result = m_PositionStrings[1].UpdateSentence(deviceContext, m_Font1, finalString, 10, 120, 1.f, 1.f, 1.f);
+      if (!result)
+	{
+	  return false; 
+	}
+    }
+
+  if (positionZ != m_previousPosition[2])
+    {
+      m_previousPosition[2] = positionZ;
+      _itoa_s(positionZ, tempString, 10);
+      strcpy_s(finalString, "Z: ");
+      strcat_s(finalString, tempString);
+      result = m_PositionStrings[2].UpdateSentence(deviceContext, m_Font1, finalString, 10, 140, 1.f, 1.f, 1.f);
+      if (!result)
+	{
+	  return false; 
+	}
+    }
+
+  if (rotationX != m_previousPosition[3])
+    {
+      m_previousPosition[3] = rotationX;
+      _itoa_s(rotationX, tempString, 10);
+      strcpy_s(finalString, "rX: ");
+      strcat_s(finalString, tempString);
+      result = m_PositionStrings[3].UpdateSentence(deviceContext, m_Font1, finalString, 10, 180, 1.f, 1.f, 1.f);
+      if (!result)
+	{
+	  return false; 
+	}
+    }
+
+  if (rotationY != m_previousPosition[4])
+    {
+      m_previousPosition[4] = rotationY;
+      _itoa_s(rotationY, tempString, 10);
+      strcpy_s(finalString, "rY: ");
+      strcat_s(finalString, tempString);
+      result = m_PositionStrings[4].UpdateSentence(deviceContext, m_Font1, finalString, 10, 200, 1.f, 1.f, 1.f);
+      if (!result)
+	{
+	  return false; 
+	}
+    }
+
+  if (rotationZ != m_previousPosition[5])
+    {
+      m_previousPosition[5] = rotationZ;
+      _itoa_s(rotationZ, tempString, 10);
+      strcpy_s(finalString, "rZ: ");
+      strcat_s(finalString, tempString);
+      result = m_PositionStrings[5].UpdateSentence(deviceContext, m_Font1, finalString, 10, 220, 1.f, 1.f, 1.f);
+      if (!result)
+	{
+	  return false; 
+	}
+    }
+  
+}
