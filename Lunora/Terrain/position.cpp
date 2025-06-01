f2dac46287b5036ef50564979460d1760cd9978f
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-06-01 23:04:57 +0300

    free camera

--- Lunora/Terrain/position.cpp
+++ Lunora/Terrain/position.cpp
@@ -23,3 +23,38 @@ Position::Position()
 }
 
 Position::Position(const Position& other) {}
+
+Position::~Position() {}
+
+void Position::SetPosition(float x, float y, float z)
+{
+  m_positionX = x;
+  m_positionY = y;
+  m_positionZ = z;
+}
+
+void Position::SetRotation(float x, float y, float z)
+{
+  m_rotationX = x;
+  m_rotationY = y;
+  m_rotationZ = z;
+}
+
+void Position::GetPosition(float& x, float& y, float& z)
+{
+  x = m_positionX;
+  y = m_positionY;
+  z = m_positionZ;
+}
+
+void PositionClass::GetRotation(float& x, float& y, float& z)
+{
+  x = m_rotationX;
+  y = m_rotationY;
+  z = m_rotationZ;
+}
+
+void Position::SetFrameTime(float time)
+{
+  m_frameTime = time; 
+}
