be9b5d36810661634ff07f99fd4c782fda799210
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-06-02 22:26:57 +0300

    position

--- Lunora/Terrain/position.cpp
+++ Lunora/Terrain/position.cpp
@@ -58,3 +58,227 @@ void Position::SetFrameTime(float time)
 {
   m_frameTime = time; 
 }
+
+void Position::MoveForward(bool keydown)
+{
+  float radians;
+  
+  if(keydown)
+    {
+      m_forwardSpeed += m_frameTime * 1.0f;
+      if(m_forwardSpeed > (m_frameTime * 50.0f))
+	{
+	  m_forwardSpeed = m_frameTime * 50.0f;
+	}
+    }
+  else
+    {
+      m_forwardSpeed -= m_frameTime * 0.5f;
+
+      if(m_forwardSpeed < 0.0f)
+	{
+	  m_forwardSpeed = 0.0f;
+	}
+    }
+
+  radians = m_rotationY * 0.0174532925f;
+  
+  m_positionX += sinf(radians) * m_forwardSpeed;
+  m_positionZ += cosf(radians) * m_forwardSpeed;
+}
+
+void Position::MoveBackward(bool keydown)
+{
+  float radians;
+
+  if (keydown)
+    {
+      m_backwardSpeed += m_frameTime * 1.f;
+
+      if (m_backwardSpeed > (m_frameTime * 50.f))
+	{
+	  m_backwardSpeed = m_frameTime * 50.f;
+	}
+    }
+  else
+    {
+      m_backwardSpeed -= m_frameTime * 0.5f;
+
+      if (m_backwardSpeed < 0.f)
+	{
+	  m_backwardSpeed = 0.f; 
+	}
+    }
+
+  radians = m_rotationY * 0.0174532925f;
+
+  m_positionX -= sinf(radians) * m_backwardSpeed;
+  m_positionZ -= cosf(radians) * m_backwardSpeed; 
+}
+
+void Position::MoveUpward(bool keydown)
+{
+  if(keydown)
+    {
+      m_upwardSpeed += m_frameTime * 1.5f;
+
+      if(m_upwardSpeed > (m_frameTime * 15.0f))
+	{
+	  m_upwardSpeed = m_frameTime * 15.0f;
+	}
+    }
+  else
+    {
+      m_upwardSpeed -= m_frameTime * 0.5f;
+
+      if(m_upwardSpeed < 0.0f)
+	{
+	  m_upwardSpeed = 0.0f;
+	}
+    }
+
+  m_positionY += m_upwardSpeed;
+}
+
+void Position::MoveDownward(bool keydown)
+{
+
+  if(keydown)
+    {
+      m_downwardSpeed += m_frameTime * 1.5f;
+
+      if(m_downwardSpeed > (m_frameTime * 15.0f))
+	{
+	  m_downwardSpeed = m_frameTime * 15.0f;
+	}
+    }
+  else
+    {
+      m_downwardSpeed -= m_frameTime * 0.5f;
+
+      if(m_downwardSpeed < 0.0f)
+	{
+	  m_downwardSpeed = 0.0f;
+	}
+    }
+
+  m_positionY -= m_downwardSpeed;
+}
+
+void Position::TurnLeft(bool keydown)
+{
+  if(keydown)
+    {
+      m_leftTurnSpeed += m_frameTime * 5.0f;
+
+      if(m_leftTurnSpeed > (m_frameTime * 150.0f))
+	{
+	  m_leftTurnSpeed = m_frameTime * 150.0f;
+	}
+    }
+  else
+    {
+      m_leftTurnSpeed -= m_frameTime* 3.5f;
+
+      if(m_leftTurnSpeed < 0.0f)
+	{
+	  m_leftTurnSpeed = 0.0f;
+	}
+    }
+
+  m_rotationY -= m_leftTurnSpeed;
+
+  if(m_rotationY < 0.0f)
+    {
+      m_rotationY += 360.0f;
+    }
+}
+
+void Position::TurnRight(bool keydown)
+{
+  if(keydown)
+    {
+      m_rightTurnSpeed += m_frameTime * 5.0f;
+
+      if(m_rightTurnSpeed > (m_frameTime * 150.0f))
+	{
+	  m_rightTurnSpeed = m_frameTime * 150.0f;
+	}
+    }
+  else
+    {
+      m_rightTurnSpeed -= m_frameTime* 3.5f;
+
+      if(m_rightTurnSpeed < 0.0f)
+	{
+	  m_rightTurnSpeed = 0.0f;
+	}
+    }
+
+  m_rotationY += m_rightTurnSpeed;
+
+  if(m_rotationY > 360.0f)
+    {
+      m_rotationY -= 360.0f;
+    }
+}
+
+void Position::LookUpward(bool keydown)
+{
+  if(keydown)
+    {
+      m_lookUpSpeed += m_frameTime * 7.5f;
+
+      if(m_lookUpSpeed > (m_frameTime * 75.0f))
+	{
+	  m_lookUpSpeed = m_frameTime * 75.0f;
+	}
+    }
+  else
+    {
+      m_lookUpSpeed -= m_frameTime* 2.0f;
+
+      if(m_lookUpSpeed < 0.0f)
+	{
+	  m_lookUpSpeed = 0.0f;
+	}
+    }
+
+  m_rotationX -= m_lookUpSpeed;
+
+	
+  if(m_rotationX > 90.0f)
+    {
+      m_rotationX = 90.0f;
+    }
+}
+
+void Position::LookDownward(bool keydown)
+{
+  if(keydown)
+    {
+      m_lookDownSpeed += m_frameTime * 7.5f;
+
+      if(m_lookDownSpeed > (m_frameTime * 75.0f))
+	{
+	  m_lookDownSpeed = m_frameTime * 75.0f;
+	}
+    }
+  else
+    {
+      m_lookDownSpeed -= m_frameTime* 2.0f;
+
+      if(m_lookDownSpeed < 0.0f)
+	{
+	  m_lookDownSpeed = 0.0f;
+	}
+    }
+
+  m_rotationX += m_lookDownSpeed;
+
+
+  if(m_rotationX < -90.0f)
+    {
+      m_rotationX = -90.0f;
+    }
+}
