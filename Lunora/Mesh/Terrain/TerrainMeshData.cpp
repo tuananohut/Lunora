1c0c74dbef1bef77ff1f2db6e61876e792894ef9
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-07-03 21:58:45 +0300

    terrain

--- Lunora/Mesh/Terrain/TerrainMeshData.cpp
+++ Lunora/Mesh/Terrain/TerrainMeshData.cpp
@@ -6,7 +6,60 @@ Terrain::Terrain()
   m_indexBuffer = nullptr; 
 }
 
+Terrain::~Terrain() {}
+
+bool Terrain::Initialize(ID3D11Device* Device)
+{
+  bool result;
+
+  result = InitializeBuffers(Device);
+  if (!result)
+    {
+      return false;
+    }
+  
+  return true;
+}
+
+void Terrain::Shutdown()
+{
+  ShutdownBuffers(); 
+}
+
 bool Terrain::InitializeBuffers(ID3D11Device* Device)
 {
+  VertexType* vertices;
+  unsigned long* indices;
+  D3D11_BUFFER_DESC vertexBufferDesc, indexBufferDesc;
+  D3D11_SUBRESOURCE_DATA vertexData, indexData;
+  HRESULT result;
+  
+  int i, j, terrainWidth, terrainHeight, index;
+  XMFLOAT4 color;
+  float positionX, positionZ;
+
+  terrainHeight = 256;
+  terrainWidth = 256;
+
+  color = XMFLOAT4(1.f, 1.f, 1.f, 1.f);
+
+  m_vertexCount = (terrainWidth - 1) * (terrainHeight - 1) * 8;
+
+  m_indexCount = m_vertexCount;
+
+  vertices = new VertexType[m_vertexCount];
+  if (!vertices)
+    {
+      return false; 
+    }
+
+  indices = new unsigned long[m_indexCount];
+  if (!indices)
+    {
+      return false;
+    }
+  
   return true; 
 }
+
+
--- Lunora/Mesh/Terrain/TerrainMeshData.h
+++ Lunora/Mesh/Terrain/TerrainMeshData.h
@@ -12,16 +12,16 @@ struct Terrain: public Mesh
 {
   Terrain();
   ~Terrain();
-
-  bool Initialize(ID3D11Device*, char*);
+  
+  bool Initialize(ID3D11Device*);
   void Shutdown();
   bool Render(ID3D11DeviceContext*);
 
   int GetIndexCount();
 
-  virtual bool InitializeBuffers(ID3D11Device*);
-  virtual void ShutdownBuffers();
-  virtual void RenderBuffers(ID3D11DeviceContext*);
+  bool InitializeBuffers(ID3D11Device*);
+  void ShutdownBuffers();
+  void RenderBuffers(ID3D11DeviceContext*);
  
   ID3D11Buffer *m_vertexBuffer, *m_indexBuffer;
   int m_vertexCount, m_indexCount;
