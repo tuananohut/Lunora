4ce4c9ad262730ea3391544685cdd754a572ff88
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-08-29 22:10:00 +0300

    vertex buffer

--- Lunora/Geometry.h
+++ Lunora/Geometry.h
@@ -1,8 +1,6 @@
 #include <DirectXMath.h>
 #include "Renderer.h"
 
-using namespace DirectX; 
-
 struct HexMeshVertexBuffer
 {
   XMFLOAT3 position;
@@ -22,3 +20,41 @@ struct MeshVertexBuffer
   XMFLOAT3 binormal; 
   XMFLOAT3 color; 
 };
+
+ID3D11Buffer* g_pVertexBuffer;
+
+struct SimpleVertexCombined
+{
+  XMFLOAT3 Pos;
+  XMFLOAT3 Col; 
+};
+
+SimpleVertexCombined verticesCombo[] =
+  {
+    XMFLOAT3(  0.f ,  0.5f,  0.5f  ),
+    XMFLOAT3(  0.f ,  0.f ,  0.5f  ),
+    XMFLOAT3(  0.5f, -0.5f,  0.5f  ),
+    XMFLOAT3(  0.5f, -0.5f,  0.5f  ),
+    XMFLOAT3( -0.5f, -0.5f,  0.5f  ),
+    XMFLOAT3(  0.f,   0.5f,  0.f   ),
+  };
+
+HRESULT CreateVertexBuffer(CoreRenderBuffers& RenderBuffers)
+{
+  HRESULT hr; 
+  
+  D3D11_BUFFER_DESC bufferDesc;
+  bufferDesc.Usage = D3D11_USAGE_DEFAULT;
+  bufferDesc.ByteWidth = sizeof( SimpleVertexCombined ) * 3;
+  bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+  bufferDesc.CPUAccessFlags = 0;
+  bufferDesc.MiscFlags = 0;
+
+  D3D11_SUBRESOURCE_DATA InitData;
+  InitData.pSysMem = verticesCombo;
+  InitData.SysMemPitch = 0;
+  InitData.SysMemSlicePitch = 0;
+
+  hr = RenderBuffers.Device->CreateBuffer( &bufferDesc, &InitData, &g_pVertexBuffer );
+}
+
