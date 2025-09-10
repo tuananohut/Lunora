18fb051a5748ef87bc906af131e74a3d92e04c04
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-09-10 21:27:08 +0300

    shader

--- Lunora/Geometry.h
+++ Lunora/Geometry.h
@@ -90,19 +90,42 @@ HRESULT CreateIndexBuffer(CoreRenderBuffers& RenderBuffers)
 void IAStage(CoreRenderBuffers& RenderBuffers)
 {
   HRESULT hr; 
-  ID3D11InputLayout* layout; 
+  ID3D11InputLayout* layout;
+  ID3D10Blob* errorMessage = nullptr;
+  ID3D10Blob* vertexShaderBuffer = nullptr;
+  ID3D10Blob* pixelShaderBuffer = nullptr;
+  D3D11_SAMPLER_DESC samplerDesc;
+  D3D11_BUFFER_DESC matrixBufferDesc;
+
+  result = D3DCompileFromFile("simple_vertex_shader.vs", NULL, NULL,
+			      "VSmain", "vs_5_0",
+			      D3D10_SHADER_ENABLE_STRICTNESS, 0,
+			      &vertexShaderBuffer, &errorMessage);
+  if (FAILED(result))
+    {
+      if (errorMessage)
+	{
+	  OutputShaderErrorMessage(errorMessage, hwnd, vsFilename);
+	}
+      else
+	{
+	  MessageBoxW(hwnd, vsFilename, L"Missing Shader File", MB_OK | MB_ICONERROR);
+	}
+
+      return false;
+    }
   
   D3D11_INPUT_ELEMENT_DESC input_layout[] =
     {
-      { L"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,
+      { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,
 	D3D11_INPUT_PER_VERTEX_DATA, 0 },
-      { L"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT,
+      { "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT,
 	D3D11_INPUT_PER_VERTEX_DATA, 0 },
     };
 
   UINT num_elements = sizeof(layout) / sizeof(layout)[0];
   
-  hr = CreateInputLayout(&input_layout, num_elements, 0, ,layout);
+  hr = RenderBuffers.Device->CreateInputLayout(&input_layout, num_elements, vertexShaderBuffer->, layout);
   
   UINT stride = sizeof( SimpleVertex );
   UINT offset = 0;
