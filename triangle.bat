44d2c785a536c1678c34d5facb7aa6108af4bb45
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-09-27 08:53:51 +0300

    clean

--- triangle.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-IF NOT EXIST triangleapp mkdir triangleapp
-pushd triangleapp
-pwd
-
-cl -EHsc -FC -Zi ..\..\Lunora\Triangle\*.cpp user32.lib Gdi32.lib d3d11.lib d3dcompiler.lib dxgi.lib ..\data\resource.res /link /SUBSYSTEM:WINDOWS /OUT:triangle.exe
-
-popd
