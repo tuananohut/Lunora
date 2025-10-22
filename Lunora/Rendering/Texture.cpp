cbb86f75ac5f49da591a52ab54e21c1bf88efa79
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-10-22 22:23:09 +0300

    texture

--- Lunora/Rendering/Texture.cpp
+++ Lunora/Rendering/Texture.cpp
@@ -16,6 +16,24 @@ bool InitializeTexture(ID3D11Device* device,
     {
       return false; 
     }
+
+  textureDesc.Height = texture.m_height;
+  textureDesc.Width = texture.m_width;
+  textureDesc.MipLevels = 0;
+  textureDesc.ArraySize = 1;
+  textureDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+  textureDesc.SampleDesc.Count = 1;
+  textureDesc.SampleDesc.Quality = 0;
+  textureDesc.Usage = D3D11_USAGE_DEFAULT;
+  textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
+  textureDesc.CPUAccessFlags = 0;
+  textureDesc.MiscFlags = D3D11_RESOURCE_MISC_GENERATE_MIPS;
+
+  hr = device->CreateTexture2D(&textureDesc, NULL, &m_texture);
+  if (FAILED(hr))
+    return false;
+
+  
 }
 
 void ReleaseTexture(Texture*)
