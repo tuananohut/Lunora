1d9d37e387bdd8b226f88094f915fd0bf72faad7
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-12-01 22:30:26 +0300

    logger

--- Lunora/Engine/src/core/logger.h
+++ Lunora/Engine/src/core/logger.h
@@ -29,4 +29,32 @@ LAPI void log_output(log_level, const char* message, ...);
 
 #define LFATAL(message, ...) log_output(LOG_LEVEL_FATAL, message, __VA_ARGS__); 
 
+#ifndef LERROR
+#define LERROR(message, ...) log_output(LOG_LEVEL_ERROR, message, __VA_ARGS__);
+#endif
+
+#if LOG_WARN_ENABLED == 1
+#define LWARN(message, ...) log_output(LOG_LEVEL_WARN, message, __VA_ARGS__);
+#else
+#define LWARN(message, ...)
+#endif 
+
+#if LOG_INFO_ENABLED == 1
+#define LINFO(message, ...) log_output(LOG_INFO_ENABLED, message, __VA_ARGS__);
+#else
+#define LINFO(message, ...)
+#endif
+
+#if LOG_DEBUG_ENABLED == 1
+#define LDEBUG(message, ...) log_output(LOG_DEBUG_ENABLED, message, __VA_ARGS__);
+#else
+#define LDEBUG(message, ...)
+#endif 
+
+#if LOG_TRACE_ENABLED == 1
+#define LTRACE(message, ...) log_output(LOG_TRACE_ENABLED, message, __VA_ARGS__);
+#else
+#define LTRACE(message, ...)
+#endif 
+
 #endif
